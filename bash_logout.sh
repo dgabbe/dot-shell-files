22de0fa8dbcd3cb0414953c8a0fb632e5eb367a2
David G <dgabbe@acm.org>
2019-03-21 09:54:54 -0400

    Update to use modern Bash syntax.

--- bash_logout.sh
+++ bash_logout.sh
@@ -2,7 +2,7 @@
 #
 # ~/.bash_logout: executed by bash(1) when login shell exits.
 # Clear the screen to increase privacy when leaving the console.
-
-if [ "$SHLVL" = 1 ]; then
-    [ -x /usr/bin/clear_console ] && /usr/bin/clear_console -q
+#
+if [[ ${SHLVL} = 1 ]]; then
+    [[ -x /usr/bin/clear_console ]] && /usr/bin/clear_console -q
 fi
